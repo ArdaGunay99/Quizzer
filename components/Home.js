e98bf564c836255d8e26274ae93d9a847c16df13
ArdaGunay99 <arda.99.gunay@gmail.com>
2020-12-12 20:34:12 +0300

    Initialized the home page with categories and icons. No functionalities yet..

--- components/Home.js
+++ components/Home.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 class Home extends Component {
   static navigationOptions = ({navigation}) => {
@@ -38,7 +39,33 @@ class Home extends Component {
   render() {
     return (
       <View style={styles.main}>
-        <Text>Home Page</Text>
+        <Text style={styles.upperText}>Select a category</Text>
+        <View style={styles.boxContainer}>
+          <TouchableOpacity  style={styles.music}>
+            <Text style={styles.categoryText}>Music</Text>
+            <Icon name="music" size={25}/>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.sports}>
+            <Text style={styles.categoryText}>Sports</Text>
+            <Icon name="football-ball" size={25}/>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.science}>
+            <Text style={styles.categoryText}>Science</Text>
+            <Icon name="radiation" size={25}/>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.art}>
+            <Text style={styles.categoryText}>Art</Text>
+            <Icon name="paint-brush" size={25}/>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.movies}>
+            <Text style={styles.categoryText}>Movies</Text>
+            <Icon name="video" size={25}/>
+          </TouchableOpacity>
+          <TouchableOpacity  style={styles.history}>
+            <Text style={styles.categoryText}>History</Text>
+            <Icon name="book" size={25}/>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -68,11 +95,90 @@ class Home extends Component {
 }
 
 const styles = StyleSheet.create({
+  upperText:{
+    fontSize:24,
+    marginTop:70,
+    marginBottom:20,
+  },
+  categoryText:{
+    fontSize: 19,
+    marginBottom: 5,
+  },
   main: {
     flex: 1,
     backgroundColor: '#e3b579',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
+    alignContent: 'space-around',
+  },
+  boxContainer: {
+    flex:2,
+    flexWrap: 'nowrap',
+    backgroundColor: '#e3b579',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    alignContent: 'space-around',
+  },
+  music:{
+    alignSelf: 'flex-start',
+    width: 70,
+    backgroundColor: 'red',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
+  },
+  sports:{
+    width: 70,
+    backgroundColor: 'skyblue',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
+  },
+  movies:{
+    width: 70,
+    backgroundColor: 'yellow',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
+  },
+  science:{
+    width: 70,
+    backgroundColor: 'orange',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
+  },
+  history:{
+    width: 70,
+    backgroundColor: 'purple',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
+  },
+  art:{
+    width: 70,
+    backgroundColor: 'lightgreen',
+    height: 70,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:20,
+    marginTop:10,
   },
 });
 
